47aaf758f25aeb2501bbebfb00e0858a5c96f9f9
unknown <jesadakorn.26447@gmail.com>
2024-10-17 08:21:20 +0700

    แก้ไขหน้าแดชบอร์ดและปรับปรุง URL

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 # URL patterns สำหรับแอปพลิเคชัน
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),  # เส้นทางสำหรับสมัครสมาชิก
     path('boss_dashboard/', views.boss_dashboard, name='boss_dashboard'),  # เส้นทางสำหรับแดชบอร์ดเจ้านาย
     path('employee_dashboard/', views.employee_dashboard, name='employee_dashboard'),  # เส้นทางสำหรับแดชบอร์ดพนักงาน
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
